cc984136adbd87b328cd60634121536a45074bcf
Chris <breakfastalways@gmail.com>
2017-11-14 15:24:08 -0500

    Added ability to share posts by email

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,57 @@
-from django.shortcuts import render, get_object_or_404
-from . import models
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 def post_list(request):
-	posts=Post.published.all()
-	return render(request, 'blog/post/list.html', {'posts': posts})
+	object_list = Post.objects.all()
+	paginator = Paginator(object_list, 3)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver the first page
+		posts = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range deliver last page of results
+		posts = paginator.page(paginator.num_pages)
+	return render(request, 'blog/post/list.html', {'posts': posts, 'page': page})
+
+class PostListView(ListView):
+	queryset = Post.objects.all()
+	context_object_name = 'posts'
+	paginate_by = 3
+	template_name = 'blog/post/list.html'
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(Post, slug=post,
-									status='published',
 									publish__year=year,
 									publish__month=month,
 									publish__day=day)
 	return render(request, 'blog/post/detail.html', {'post': post})
+
+def post_share(request, post_id):
+	#Retrieve post by id
+	post = get_object_or_404(Post, id=post_id)
+	sent = False
+
+	if request.method == 'POST':
+		#Form was submitted
+		form = EmailPostForm(request.POST)
+		if form.is_valid():
+			#Form fields passed validation
+			cd = form.cleaned_data
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = '{} ({}) recommends you read "{}"'.format(cd['name'], cd['email'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['comments'])
+			send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+			sent = True
+			return render(request, 'blog/post/list.html')
+		else:
+			form = EmailPostForm()
+		return render(request, 'blog/post/share.html', {'post': post,
+														'form': form,
+														'sent': sent})
+	return render(request, 'blog/post/share.html')
--- config/settings.py
+++ config/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'xchriscrossed@gmail.com'
+EMAIL_HOST_PASSWORD = 'Reverend1'
+EMAIL_PORT = 8000
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
